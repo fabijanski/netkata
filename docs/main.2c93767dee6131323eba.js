a6801056399713bff162d20c3be46abf5ac3d971
fabijanski <mm.fabijanski@gmail.com>
2017-09-14 03:48:10 +0200

    Add docs folder. Change readme file.

---
+++ docs/main.2c93767dee6131323eba.js
@@ -0,0 +1,2 @@
+webpackJsonp([0],[,function(o,n,c){"use strict";c(2),c(3)},function(o,n){},function(o,n,c){"use strict";console.log("Hello world!")}],[1]);
+//# sourceMappingURL=main.2c93767dee6131323eba.js.map
